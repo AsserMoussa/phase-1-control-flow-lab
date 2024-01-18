c1a1a6e1657505a0b6b2049dedd2bec8b00e454a
assermoussa <asser.y.moussa@gmail.com>
2024-01-17 19:13:33 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue<401){
+    return "This one is on me!"
+  }
+  else if (someValue>400 && someValue<2000){
+    return "That will be twenty bucks."
+  }
+  else if(someValue>2000 && someValue<2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let citycheck; 
+  city == "NYC" ? (citycheck = "Ok, sounds good.") : (citycheck = "No go.");
+  return citycheck;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case 'generous':
+        response = "Thank you so much.";
+        break;
+    case 'not as generous':
+        response = "Thank you.";
+        break;
+    default:
+        response = "Bye.";
+        break;
+}
+return response;
 }
